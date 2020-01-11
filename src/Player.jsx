5ba693183a94fb19aa06c19495955d56d9c0a881
jensnyder <jennifer.snyder@vistaprint.com>
2020-01-11 14:43:02 -0500

    Add multiplier to player cards

--- src/Player.jsx
+++ src/Player.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import './Player.scss';
 
 function Player(props) {
-  const { name, position, team, score } = props;
+  const { name, position, team, score, multiplier } = props;
+  const multiplierClasses=`player__multiplier player__multiplier--${multiplier}`;
   return (
     <li className="player">
       <div className="player__data">
@@ -17,8 +18,13 @@ function Player(props) {
             {team}
           </div>
         </div>
-        <div className="player__score">
-          {score}
+        <div className="player__score-info">
+          <span className={multiplierClasses}>
+            {multiplier}X BONUS
+          </span>
+          <span className="player__score">
+            {score}
+          </span>
         </div>
       </div>
     </li>
--- src/Roster.jsx
+++ src/Roster.jsx
@@ -7,13 +7,14 @@ function Roster({ weekData }) {
   return (
     <ul className="roster">
       {weeklyRoster.map(player => {
-        const { roster_slot, player_name, position, team, score } = player;
+        const { roster_slot, player_name, position, team, score, multiplier } = player;
         return <Player
           key={roster_slot}
           name={player_name}
           position={position}
           team={team}
           score={score}
+          multiplier={multiplier}
         />
       })}
     </ul>
