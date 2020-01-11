4f0c02b03553704f504bfe77ea112da61dd09033
jensnyder <jennifer.snyder@vistaprint.com>
2020-01-11 16:09:18 -0500

    Add team colors to player cards

--- src/Player.jsx
+++ src/Player.jsx
@@ -3,9 +3,10 @@ import './Player.scss';
 
 function Player(props) {
   const { name, img, position, team, score, multiplier } = props;
-  const multiplierClasses=`player__multiplier player__multiplier--${multiplier}`;
+  const playerClasses = `player player--${team}`
+  const multiplierClasses =`player__multiplier player__multiplier--${multiplier}`;
   return (
-    <li className="player">
+    <li className={playerClasses}>
       <div className="player__data">
         <div className="player__position">
           {position}
