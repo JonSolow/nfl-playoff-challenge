62fbbddf464c64d3a36a57c19b02a5425c2bea63
jensnyder <jlsnyder09@gmail.com>
2021-01-02 12:24:35 -0500

    Restyle header and buttons

--- src/Button.js
+++ src/Button.js
@@ -5,8 +5,8 @@ function Button(props) {
   const { name, number, disabled, selected, setSelectedWeek } = props;
   return (
     <button
-      className={`button--selected-${selected}`}
-      onClick={() => {setSelectedWeek(number); window.scrollTo(0, 0)}}
+      className={`button button--selected-${selected}`}
+      onClick={() => { setSelectedWeek(number); window.scrollTo(0, 0) }}
       disabled={disabled}
     >
       {name}
